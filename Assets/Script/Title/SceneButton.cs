7159661baac5f2ca6c410d33621c885eca87d9fe
AkaneShibata <raccoon106@gmail.com>
2021-02-07 16:32:00 +0900

    シーンの追加、素材画像の追加、TOP画面のスタートボタン作成

---
+++ Assets/Script/Title/SceneButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButton : MonoBehaviour
+{
+    public void OnClickTopStartButton()
+    {
+        Debug.Log("OnClickTopStartButton");
+        SceneManager.LoadScene("StageList");
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
